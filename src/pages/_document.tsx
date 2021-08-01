257ef7775af8b77471812df0e22051bcb30eea73
shujathkhan <shujathlive@gmail.com>
2021-08-02 00:24:09 +0530

    🚀 Deploy on docker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,27 +5,20 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
-import { extractCritical } from '@emotion/server';
 import { resetServerContext } from 'react-beautiful-dnd';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const page = await ctx.renderPage();
     const initialProps = await Document.getInitialProps(ctx);
-    const styles = extractCritical(page.html);
     resetServerContext();
-    return { ...initialProps, ...page, ...styles };
+    return { ...initialProps, ...page };
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <style
-            data-emotion-css={this.props.ids.join(' ')}
-            dangerouslySetInnerHTML={{ __html: this.props.css }}
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
